94a7575b007b6d3714284383206b14407202fbd1
Dan33y <enderda33y@gmail.com>
2021-07-10 11:50:44 +0200

    Initial commit

--- src/main/java/de/areon/system/listener/test2.java
+++ src/main/java/de/areon/system/listener/test2.java
@@ -1,6 +1,13 @@
 package de.areon.system.listener;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 
-public class test2 implements Listener {
+public class test2 implements Listener, CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        return false;
+    }
 }
